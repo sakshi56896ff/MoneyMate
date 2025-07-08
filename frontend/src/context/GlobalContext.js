b16d069f6101550dce9fdadae9774db7f949c8e3
sakshi56896ff <157792598+sakshi56896ff@users.noreply.github.com>
2025-07-09 00:11:09 +0530

    Fix: use dynamic PORT for Render

--- frontend/src/context/GlobalContext.js
+++ frontend/src/context/GlobalContext.js
@@ -1,7 +1,7 @@
 import React, { useState, useContext } from 'react';
 import axios from 'axios';
 
-const BASE_URL = "http://localhost:5000/api/v1/";
+const BASE_URL = process.env.REACT_APP_BASE_URL;
 
 const GlobalContext = React.createContext();
 
