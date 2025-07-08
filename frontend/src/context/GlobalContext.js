f4fd8e0684f2a63f3f262f20008eea6183e8a9a3
sakshi56896ff <157792598+sakshi56896ff@users.noreply.github.com>
2025-07-09 00:59:56 +0530

    update frontend

--- frontend/src/context/GlobalContext.js
+++ frontend/src/context/GlobalContext.js
@@ -1,7 +1,9 @@
 import React, { useState, useContext } from 'react';
 import axios from 'axios';
+const BASE_URL = "https://moneymate-cu8b.onrender.com/api/v1/";
 
-const BASE_URL = process.env.REACT_APP_BASE_URL;
+
+
 
 const GlobalContext = React.createContext();
 
